b74b03ead7e368161eb812ebe648b95ac8acd528
OrrMatzkin <orr.matzkin@gmail.com>
2022-04-06 10:11:49 +0300

    implemented the polynomial fitting estimator

--- IMLearn/learners/regressors/polynomial_fitting.py
+++ IMLearn/learners/regressors/polynomial_fitting.py
@@ -5,7 +5,7 @@ from ...base import BaseEstimator
 import numpy as np
 
 
-class PolynomialFitting(BaseEstimator):
+class PolynomialFitting(LinearRegression):
     """
     Polynomial Fitting using Least Squares estimation
     """
@@ -18,8 +18,8 @@ class PolynomialFitting(BaseEstimator):
         k : int
             Degree of polynomial to fit
         """
-        super().__init__()
-        raise NotImplementedError()
+        super().__init__(include_intercept=False)  # the basis functions defines h1(x)=1 so the intercept is included
+        self.k = k
 
     def _fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
         """
@@ -33,7 +33,8 @@ class PolynomialFitting(BaseEstimator):
         y : ndarray of shape (n_samples, )
             Responses of input data to fit to
         """
-        raise NotImplementedError()
+        vand_X = self.__transform(X)
+        super().fit(vand_X, y)
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -49,7 +50,8 @@ class PolynomialFitting(BaseEstimator):
         responses : ndarray of shape (n_samples, )
             Predicted responses of given samples
         """
-        raise NotImplementedError()
+        vand_X = self.__transform(X)
+        return super().predict(vand_X)
 
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
@@ -68,7 +70,8 @@ class PolynomialFitting(BaseEstimator):
         loss : float
             Performance under MSE loss function
         """
-        raise NotImplementedError()
+        vand_X = self.__transform(X)
+        return super().loss(vand_X, y)
 
     def __transform(self, X: np.ndarray) -> np.ndarray:
         """
@@ -83,4 +86,4 @@ class PolynomialFitting(BaseEstimator):
         transformed: ndarray of shape (n_samples, k+1)
             Vandermonde matrix of given samples up to degree k
         """
-        raise NotImplementedError()
+        return np.vander(X, N=self.k+1, increasing=True)
